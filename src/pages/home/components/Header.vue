46c433c6de0b8d40f89d064cb880561b26e7c86f
涎涎 <511948469@qq.com>
2019-04-08 15:30:26 +0800

    add header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="header">
+    <div class="header-left">
+        <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+        <span class="iconfont">&#xe632;</span>
+        输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+        <span class="iconfont">&#xe64a;</span>
+        城市
+    </div>
+</div>
+</template>
+<script>
+export default {
+  name:'homeHeader'
+}
+</script>
+<style lang="stylus" scoped>
+// @import '~@/assets/styles/varibles.styl';
+@import '~styles/varibles.styl';
+.header
+   display:flex
+   line-height:.86rem
+   background:$bgColor
+   color:#fff
+   .header-left
+     width:0.64rem
+     float: left
+     .back-icon
+       text-align:center
+       font-size:.4rem
+   .header-input 
+     flex:1
+     height:0.64rem
+     line-height:0.64rem
+     margin-top:.12rem
+     margin-left:.2rem
+     padding-left:.2rem
+     background:#ffffff
+     border-radius:.1rem
+     color:#ccc
+   .header-right
+     width:1.24rem
+     float:right
+     text-align:center
+</style>
+
--- whereTravel/src/main.js
+++ whereTravel/src/main.js
@@ -3,13 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import fastclick from 'fastclick'
-import 'styles/reset.css'
-import 'styles/border.css'
-import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
-fastclick.attach(document.body)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
